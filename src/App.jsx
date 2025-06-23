0ff7e0360e8e304d26bf7bfbcb4a32f7a19ffd5d
Ondreev <mrtutanhamon@gmail.com>
2025-06-23 20:04:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from 'react'
+import { ProductList } from './components/ProductList'
 
 function App() {
   const [products, setProducts] = useState([])
+  const [cartItems, setCartItems] = useState([])
+
+  const addToCart = (product) => {
+    setCartItems((prev) => [...prev, product])
+    console.log('🛒 Добавлено в корзину:', product.name)
+  }
 
   useEffect(() => {
     fetch('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjKj32Emszk_aVk3GKB9aY23lQG6Nkxt4Va2WAe710KiwsXevhrLS2w7DNIs6jEVXFTh6LwZc6U3MvXT19YvV1TgTUEbexYkzsfGQFFjWFZAVAFZKiDq5FHD8fE3Lkj9dZ7EyhkfuYmc_IoK0TgM73Q4CjX4kTy5cdQJEMXt15l4HE-48yp-k4wiDaXbvVVA1YPSXAC8gbarNjKKHNKWlZr6V4CZg_qGs_ykX33J6rXJ4fc1G5deHFdDLxMIC8OmNNreLx-E-V0C_knBjPSDIn0owFxdxD9qCQ0LXYKcmegJta6Kf5PXc6AwIcyig&lib=MV5zt_1yAKnbl-Cizom2A6UBf2lQkhfMZ')
@@ -23,23 +30,7 @@ function App() {
   return (
     <div className="max-w-xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">JWD Express</h1>
-      <div className="grid gap-4">
-        {products.map(product => (
-          <div key={product.id} className="border rounded p-4 shadow">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="w-full h-40 object-cover mb-2"
-            />
-            <h2 className="text-lg font-semibold">{product.name}</h2>
-            <p className="text-sm text-gray-600">{product.description}</p>
-            {product.promo && (
-              <span className="text-red-500 font-bold text-sm">АКЦИЯ!</span>
-            )}
-            <p className="mt-2 font-bold">{product.price}₽</p>
-          </div>
-        ))}
-      </div>
+      <ProductList products={products} addToCart={addToCart} discountRules={[]} />
     </div>
   )
 }
