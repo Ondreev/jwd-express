750feab23262e856efe06bb8dad6da3072360600
Ondreev <mrtutanhamon@gmail.com>
2025-06-23 16:04:47 +0300

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,18 +2,19 @@ import { ProductCard } from './ProductCard'
 
 export function ProductList({ products, addToCart, discountRules }) {
   if (!Array.isArray(products)) {
-    return <p className="text-red-500">Ошибка загрузки товаров</p>
+    console.warn('❌ products не массив:', products)
+    return <p>Ошибка загрузки товаров</p>
   }
 
   const maxRule = discountRules.sort((a, b) => b.percent - a.percent)[0] || { percent: 0 }
 
   return (
-    <div className="grid grid-cols-1 gap-4">
+    <div className="grid gap-4">
       {products.map(product => (
         <ProductCard
           key={product.id}
           product={product}
-          maxDiscountPercent={maxRule.percent}
+          maxDiscount={maxRule.percent}
           addToCart={addToCart}
         />
       ))}
