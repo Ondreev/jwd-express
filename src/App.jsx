05ce25f79a6bdd32e22d97278dfaaa086b8ba67a
Ondreev <mrtutanhamon@gmail.com>
2025-06-24 23:01:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
   }, [])
 
   return (
-    <div className="max-w-xl mx-auto p-4">
+    <div className="min-h-screen bg-gray-900 text-white p-4">
       <h1 className="text-3xl font-bold text-white mb-6">JWD Express</h1>
 
       <ProductList
