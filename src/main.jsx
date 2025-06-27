c7833c0a177a1e5ce8d3d760e7f9b3d9a66fe5b7
Ondreev <mrtutanhamon@gmail.com>
2025-06-27 15:19:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import App from './App'
+import { AdminPanel } from './components/AdminPanel' // Убедись, что он есть
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
